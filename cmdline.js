2c9400d1efeb91f86ec24e7364eb07fbdd4a2784
Joe <josepha48@gmail.com>
2012-08-29 11:45:05 -0700

    initial commit of my custom version of a jsmeter web interface

---
+++ cmdline.js
@@ -0,0 +1,37 @@
+	(function () {
+		var verbose = false;
+		var args = process.argv;
+		var fs = require("fs");
+		var meter = require("jsmeter");
+                meter = meter['jsmeter'];
+		for (var a = 2; a < args.length; a++) {
+			if (args[a] === "--verbose") {
+				verbose = true;
+				continue;
+			}
+			fs.readFile(args[a], "utf8", (function (name) {
+				return function (err, data) {
+					if (err) throw err;
+					var result = meter.run(data, name.match(/([^\/])\.js$/)[1]);
+					for (var i = 0; i < result.length; i++) {
+						if (verbose) {
+							console.dir(result[i]);
+						}
+						console.log(" line start: %d", result[i].lineStart);
+						console.log(name, result[i].name.replace(/^\[\[[^\]]*\]\]\.?/, ""));
+						console.log(" statements: %d", result[i].s);
+						console.log(" lines:      %d", result[i].lines);
+						console.log(" comments:   %d", result[i].comments);
+						console.log(" % comments:   %d", Math.round(result[i].comments / (result[i].lines) * 10000)/100 + "%" );
+						console.log(" branches: %d", result[i].b);
+						console.log(" depth: %d", result[i].branchDepth);
+						console.log(" complexity: %d", result[i].complexity);
+						console.log(" Halstead Volume: %d", result[i].halsteadVolume);
+						console.log(" Halstead Potential: %d", result[i].halsteadPotential);
+						console.log(" Program Level: %d", result[i].halsteadLevel);
+						console.log(" MI Volume: %d", result[i].mi);
+					}
+				};
+			})(args[a]));
+		}
+	})();
---
+++ runjsmeter.js
@@ -0,0 +1,99 @@
+var express         = require('express');
+var fs         = require('fs');
+
+var app = express();
+
+var debug = false;
+
+function readFile(req, res) {
+    var filename = req.originalUrl;
+    filename = filename.replace(/^\//, '');
+    if ( filename === '' ) { 
+        filename = "index.html";
+    }
+    fs.readFile(filename, (function (name) {
+        return function (err, data) {
+            if (err) { 
+                res.status(404);
+                console.log(err); return; 
+            }
+            if ( filename.lastIndexOf(".js") !== -1 ) {
+                res.set('Content-Type', 'text/javascript');
+            } else if ( filename.lastIndexOf(".css") !== -1 ) {
+                res.set('Content-Type', 'text/css');
+            } else if ( filename.lastIndexOf(".png") !== -1 ) {
+                res.set('Content-Type', 'image/png');
+            } else if ( filename.lastIndexOf(".html") !== -1 ) {
+                res.set('Content-Type', 'text/html');
+            }
+            res.send(data);
+        }
+    })());
+}
+
+app.get(/^\/*/, readFile);
+
+function buildResponse(result) { 
+    
+    var name, response = "[";
+    var len = result.length;
+    
+    if ( debug ) {
+        console.log("Result length = " + len);
+    }
+    
+    for (var i = 0; i < len; i++) {
+        name = result[i].name.replace(/^\[\[[^\]]*\]\]\.?/, "Anonymous");
+        response += '{ "functionName": "' + result[i].name.replace(/^\[\[[^\]]*\]\]\.?/, "Anonymous.").replace("[[code]].", "") + '",';
+        response += '"lineStart":"' + result[i].lineStart + '",';
+        response += '"statements":"' + result[i].s + '",';
+        response += '"b":"' + result[i].b + '",';
+        response += '"lines":"' + result[i].lines + '",';
+        response += '"comments":"' + result[i].comments + '",';
+        response += '"commentPercent":"' + Math.round(result[i].comments / (result[i].lines) * 10000)/100 + '",';
+        response += '"blockDepth":"' + result[i].blockDepth + '",';
+        response += '"complexity":"' + result[i].complexity + '",';
+        response += '"halsteadVolume":"' + result[i].halsteadVolume + '",';
+        response += '"halsteadPotential":"' + result[i].halsteadPotential + '",';
+        response += '"halsteadLevel":"' + result[i].halsteadLevel + '",';
+        if ( isNaN(result[i].mi) ) { result[i].mi = 0; }
+        response += '"mi":"' + parseFloat(result[i].mi, 10) + '" }';
+        if ( i < len - 1 ) { response += ","; }
+        
+    }
+    
+    response += "]";
+    if ( debug ) {
+      console.log(response);
+    }
+    return response;
+}
+
+app.post('/node/jsmeter', function(req, res){
+
+    var data, fs = require("fs"), 
+        meter = require("jsmeter");
+    
+    meter = meter['jsmeter'];
+
+    data = "";
+    req.on('data', function(chunk) { 
+            data += chunk.toString();
+            if ( debug ) {
+                console.log(data);
+            }
+    });
+    
+    req.on('end', function() { 
+        
+        var result = meter.run(data);
+        
+        res.set('Content-Type', 'text/html');
+        
+        var response = buildResponse(result);
+        
+        res.send(response);        
+    });
+});
+
+app.listen(12000);
