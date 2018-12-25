f79d7891eaa26b014bbb5fa4be4aca3fea9fda27
Joe <josepha48@gmail.com>
2018-12-24 21:03:05 -0800

    move code to base parser ro parse the AST and get functions out of it

--- eparser.js
+++ eparser.js
@@ -1,6 +1,8 @@
 const fs = require('fs'),
-    espree = require('espree'), 
+    espree = require('espree'),
     estraverse = require('estraverse');
+
+const parser = require('./lib/baseParser');
 
 const file = process.argv[2];
 
@@ -10,60 +12,14 @@ const parserOptions = {
     ecmaVersion: 8,
     range: true,
     loc: true,
+    comment: true,
     tokens: false,
     sourceType: "module"
 };
 
 const tree = espree.parse(data, parserOptions);
 
-const functionMap = [];
-
-estraverse.traverse(tree, {
-    enter: function (node, parent) {
-        if (node.type == 'FunctionExpression') {
-            // need to look at parent 
-            const left = parent.left;
-            if ( left && left.name ) { 
-                const feNode = Object.assign({}, { name: left.name }, node);
-                functionMap.push(feNode);
-            } else if ( left && left.object && left.property && left.object.type && left.property.name ) { 
-                let prefix = left.object.type, 
-                    name = left.property.name;
-                if ( left.object.type === 'ThisExpression' ) {
-                    prefix = 'this';
-                } else if ( left.object.type === 'CallExpression' ) {
-                    const callObject = left.object.callee;
-                    prefix = `${callObject.object.name}.${callObject.property.name}`;
-                } else if ( left.object.type === 'MemberExpression' ) {
-                    const callObject = left.object;
-                    prefix = `${callObject.object.name}.${callObject.property.name}`;
-                } else {
-                console.log('Start FE Parent -------------------------');
-                console.log(left);
-                console.log('End FE Parent -------------------------');
-                } 
-                const feNode = Object.assign({}, { name: `${prefix}.${left.property.name}` }, node);
-                functionMap.push(feNode);
-            } else if ( parent.id && parent.id.name ) { 
-                const feNode = Object.assign({}, { name: parent.id.name }, node);
-                functionMap.push(feNode);
-            } else {
-                console.log('Start FE Parent -------------------------');
-                console.log(parent);
-                console.log('End FE Parent -------------------------');
-                functionMap.push(node);
-            } 
-        } else if (node.type == 'FunctionDeclaration') {
-            const feNode = Object.assign({}, { name: node.id.name }, node);
-            functionMap.push(feNode);
-        }
-    },
-    leave: function (node, parent) {
-        if (node.type == 'FunctionExpression' || node.type == 'FunctionDeclaration') {
-            //console.log('END -------------------------');
-        }
-    }
-});
+const functionMap = parser.parse(data);
 
 /* 
  data to gather
@@ -75,20 +31,22 @@ estraverse.traverse(tree, {
  - branches: 0          
  - depth: 0        
  - complexity: 1  
+   = complexity =  exits − N P 
+   = https://en.wikipedia.org/wiki/Cyclomatic_complexity
  - Halstead Volume: 15.8     
  - Halstead Potential: 8      
  - Program Level: 0.506      
  - MI Volume: 150.1
 */
-const computedResults = functionMap.map( item => {
-    
+const computedResults = functionMap.map(item => {
+
     const result = {
         file: file,
         functionName: item.name,
         startLine: item.loc.start.line,
         endLine: item.loc.end.line,
-        lines: ( item.loc.end.line - item.loc.start.line)
-    }; 
+        lines: (item.loc.end.line - item.loc.start.line)
+    };
 
     console.log('BEGIN -------------------------');
     console.log(item.body);
@@ -97,5 +55,3 @@ const computedResults = functionMap.map( item => {
 
     return result;
 });
-
-
